bc65c22d6ff828bb653d49fa0ef375bb5c9b694f
Arturo <41492101+Arti-Code@users.noreply.github.com>
2024-01-02 14:29:28 +0100

    clean main.rs

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ fn main() {
                 Ok(dir) => {
                     let path = Path::new(dir.parent().unwrap());
                     let img_location = path.join(&image_name);
-                    //println!("[IMAGE]: {}", img_location.to_str().unwrap());
                     process_image(img_location.as_path());
                 },
             }
@@ -29,13 +28,11 @@ fn main() {
 
 fn process_image(image_path: &Path) {
     let mut icon_dir = ico::IconDir::new(ico::ResourceType::Icon);
-    //println!("[IMAGE TO OPEN]: {}", image_path.to_str().unwrap());
     let file = std::fs::File::open(image_path.file_name().unwrap()).unwrap();
     let image = ico::IconImage::read_png(file).unwrap();
     icon_dir.add_entry(ico::IconDirEntry::encode(&image).unwrap());
     let image_name = image_path.file_stem().unwrap();
     let icon_str = format!("{}.ico", image_name.to_str().unwrap());
-    //println!("[ICON]: {}", &icon_str);
     let icon_name = Path::new(&icon_str);
     let file = std::fs::File::create(icon_name).unwrap();
     match icon_dir.write(file) {
